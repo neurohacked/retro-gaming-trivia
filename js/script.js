8112a3c34f411ddf1b892ef2c59e872f6be4c222
Patrick <patrick@unseenvision.com>
2016-07-31 21:54:56 -0700

    change function for creating questions and adjust check

--- js/script.js
+++ js/script.js
@@ -1,24 +1,40 @@
 $(document).ready(function() {
     // FUNCTIONS
-    // ---------------------------------------------------------------------
-    // Constructor for questions/answers
-    function quiz(name, question, wrong1, wrong2, wrong3, right, gif) {
+    // -----------------------------------------------------------------------
+    // Constructor for questions/answers -------------------------------------
+    function Quiz(name, options, gif) {
         this.name = name;
-        this.question = question;
-        this.wrong1 = wrong1;
-        this.wrong2 = wrong2;
-        this.wrong3 = wrong3;
-        this.right = right;
+        this.question = options.question;
+        this.answers = options.answers; // Array of answers
+        this.correctAnswer = options.answers[options.right];
         this.gif = queryURL = "https://api.giphy.com/v1/gifs/search?q=" + this.name + "&api_key=dc6zaTOxFJmzC";
         return this;
     }
-    // Create the questions for the game
-    function createQuestions() {
-        mario = new quiz('mario-yoshi', 'Who is Mario\'s dinosaur friend?', 'Rex', 'Rawr', 'Toshi', 'Yoshi');
-        sonic = new quiz("sonic+the+hedgehog", "What does Sonic the Hedgehog love to collect?", 'Jewels', 'Coins', 'Chili Dogs', 'Rings');
-        link = new quiz('What is the name of Link\'s fairy companion?', 'Cortana', 'Alexa', 'Siri', 'Navi');
-    }
-    // Countdown timer for each question
+    // Create the questions for the game -------------------------------------
+    var options = {
+        question: 'Who is Mario\'s dinosaur friend?',
+        answers: ['Rex', 'Rawr', 'Toshi', 'Yoshi'],
+        right: 3
+    };
+    var mario = new Quiz('mario-yoshi', options);
+
+    options = {
+        question: 'What does Sonic the Hedgehog love to collect?',
+        answers: ['Jewels', 'Coins', 'Chili Dogs', 'Rings'],
+        right: 3
+    };
+    var sonic = new Quiz("sonic-running", options);
+    // Check for correct answer ----------------------------------------------
+    Quiz.prototype.checkAnswer = function(submittedAnswer) {
+	if (this.correctAnswer === submittedAnswer) {
+		console.log('Correct!');
+		return true;
+	} else {
+		console.log('Wrong!');
+		return false;
+	}
+}
+    // Countdown timer for each question -------------------------------------
     function timer() {
         counter = setInterval(decrement, 1000);
     }
@@ -50,15 +66,14 @@ $(document).ready(function() {
     function shuffleQuestion() {
         // Display a random qustion with its available answers
         $('#question-text').html(mario.question);
-        $('#wrong-1').html(mario.wrong1);
-        $('#wrong-2').html(mario.wrong2);
-        $('#wrong-3').html(mario.wrong3);
-        $('#correct').html(mario.right);
+        $('#wrong-1').html(mario.answers[0]);
+        $('#wrong-2').html(mario.answers[1]);
+        $('#wrong-3').html(mario.answers[2]);
+        $('#correct').html(mario.answers[3]);
     }
 
     //Shuffle answers
     // Display answers to selected question in a random order.
-        for (var i=0; i<)
 
     // Display answer
     // Show a page displaying the correct answer with a related gif
@@ -96,18 +111,16 @@ $(document).ready(function() {
         $('#game-display').hide();
         $('#answer').hide();
 
-        createQuestions();
         shuffleQuestion();
 
         // Testing Console
         console.log(mario);
         console.log(sonic);
-        console.log(link);
     }
 
     // PROCESSES
-    // ---------------------------------------------------------------------
-    // When Start is clicked, display the game and start the timer.
+    // -----------------------------------------------------------------------
+    // When Start is clicked, display the game and start the timer -----------
     $('#start-game').on('click', function() {
         $('#game-display').show();
         $('#start').hide();
@@ -115,16 +128,14 @@ $(document).ready(function() {
     });
     // Check if selected answer is wrong/right and increase numWrong/numRight
     $('.answer').on('click', function() {
-        if (this.value === 'wrong') {
-            // Add to numWrong
-            numWrong++;
-            // Testing console
-            console.log('Wrong: ' + numWrong);
-        } else {
-            // Add to numRight
+        if (this.id === 'correct') {
             numRight++;
             // Testing console
             console.log('Right: ' + numRight);
+        } else {
+            numWrong++;
+            // Testing console
+            console.log('Wrong: ' + numWrong);
         }
         // Display the answer
         displayAnswer();
