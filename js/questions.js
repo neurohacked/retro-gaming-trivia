894982f0ae597af7b890254a32a6b96621a7e183
Patrick <patrick@unseenvision.com>
2016-08-02 22:54:06 -0700

    move nextQuestion into questions js file

--- js/questions.js
+++ js/questions.js
@@ -50,3 +50,64 @@ let questions = [
 ]
 
 new Quiz(questions);
+
+// Switch to next available question --------------------------------
+function nextQuestion() {
+    if (answeredQuestions === 6) {
+        displayResults();
+    } else if (answeredQuestions === 5) {
+        $('#question-text').html(availableQuestions[5].question.text);
+        answer = availableQuestions[5].question.correctAnswer
+        for (let i = 0; i < availableQuestions[5].question.answers.length; i++) {
+            j = $('<button>');
+            j.addClass('btn btn-md btn-default btn-block answer');
+            j.text(availableQuestions[5].question.answers[i]);
+            $('#answers').append(j);
+        }
+    } else if (answeredQuestions === 4) {
+        $('#question-text').html(availableQuestions[4].question.text);
+        answer = availableQuestions[4].question.correctAnswer
+        for (let i = 0; i < availableQuestions[4].question.answers.length; i++) {
+            j = $('<button>');
+            j.addClass('btn btn-md btn-default btn-block answer');
+            j.text(availableQuestions[4].question.answers[i]);
+            $('#answers').append(j);
+        }
+    } else if (answeredQuestions === 3) {
+        $('#question-text').html(availableQuestions[3].question.text);
+        answer = availableQuestions[3].question.correctAnswer
+        for (let i = 0; i < availableQuestions[3].question.answers.length; i++) {
+            j = $('<button>');
+            j.addClass('btn btn-md btn-default btn-block answer');
+            j.text(availableQuestions[3].question.answers[i]);
+            $('#answers').append(j);
+        }
+    } else if (answeredQuestions === 2) {
+        $('#question-text').html(availableQuestions[2].question.text);
+        answer = availableQuestions[2].question.correctAnswer
+        for (let i = 0; i < availableQuestions[2].question.answers.length; i++) {
+            j = $('<button>');
+            j.addClass('btn btn-md btn-default btn-block answer');
+            j.text(availableQuestions[2].question.answers[i]);
+            $('#answers').append(j);
+        }
+    } else if (answeredQuestions === 1) {
+        $('#question-text').html(availableQuestions[1].question.text);
+        answer = availableQuestions[1].question.correctAnswer
+        for (let i = 0; i < availableQuestions[1].question.answers.length; i++) {
+            j = $('<button>');
+            j.addClass('btn btn-md btn-default btn-block answer');
+            j.text(availableQuestions[1].question.answers[i]);
+            $('#answers').append(j);
+        }
+    } else {
+        $('#question-text').html(availableQuestions[0].question.text);
+        answer = availableQuestions[0].question.correctAnswer;
+        for (let i = 0; i < availableQuestions[0].question.answers.length; i++) {
+            j = $('<button>');
+            j.addClass('btn btn-md btn-default btn-block answer');
+            j.text(availableQuestions[0].question.answers[i]);
+            $('#answers').append(j);
+        }
+    }
+}
