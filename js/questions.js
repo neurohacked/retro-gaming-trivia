bf7213e4451e7c84ca4075a1f7355233c93e24dd
Patrick <patrick@unseenvision.com>
2016-08-03 00:06:26 -0700

    switch between question and answer automatically

--- js/questions.js
+++ js/questions.js
@@ -4,110 +4,48 @@ function Quiz(questions) {
     return this;
 }
 // Create the questions for the game ---------------------------------------
-let questions = [
-	{question:
-		{text: 'Who is Mario\'s dinosaur friend?',
+let questions = [{
+    question: {
+        text: 'Who is Mario\'s dinosaur friend?',
         gif: "https://api.giphy.com/v1/gifs/search?q=90s-nintendo-super-mario&api_key=dc6zaTOxFJmzC",
-        answers: ['Rex','Luigi','Toshi','Yoshi'],
-		correctAnswer: 'Yoshi'
-		},
+        answers: ['Rex', 'Luigi', 'Toshi', 'Yoshi'],
+        correctAnswer: 'Yoshi'
     },
-    {question:
-		{text: 'What does Sonic the Hedgehog love to collect?',
+}, {
+    question: {
+        text: 'What does Sonic the Hedgehog love to collect?',
         gif: "https://api.giphy.com/v1/gifs/search?q=sonic-the-hedgehog-boom-gif&api_key=dc6zaTOxFJmzC",
         answers: ['Jewels', 'Coins', 'Diamonds', 'Rings'],
-		correctAnswer: 'Rings'
-		},
+        correctAnswer: 'Rings'
     },
-    {question:
-		{text: 'Which is these is NOT a Pac-Man ghost.',
+}, {
+    question: {
+        text: 'Which is these is NOT a Pac-Man ghost.',
         gif: "https://api.giphy.com/v1/gifs/search?q=pacman-ghosts&api_key=dc6zaTOxFJmzC",
         answers: ['Inky', 'Clyde', 'Blinky', 'Bonnie'],
-		correctAnswer: 'Bonnie'
-		},
+        correctAnswer: 'Bonnie'
     },
-    {question:
-		{text: 'Who is Link\'s fairy companion?',
+}, {
+    question: {
+        text: 'Who is Link\'s fairy companion?',
         gif: "https://api.giphy.com/v1/gifs/search?q=legend-of-zelda-hey&api_key=dc6zaTOxFJmzC",
         answers: ['Siri', 'Alexa', 'Cortana', 'Navi'],
-		correctAnswer: 'Navi'
-		},
+        correctAnswer: 'Navi'
     },
-    {question:
-		{text: 'Which game franchise, born in 1992, courted controversy with its ‘Fatalities’?',
+}, {
+    question: {
+        text: 'Which game franchise, born in 1992, courted controversy with its ‘Fatalities’?',
         gif: "https://api.giphy.com/v1/gifs/search?q=dance-mash-up-mortal-kombat&api_key=dc6zaTOxFJmzC",
         answers: ['Art of Fighting', 'Fatal Fury', 'King of Fighters', 'Mortal Kombat'],
-		correctAnswer: 'Mortal Kombat'
-		},
+        correctAnswer: 'Mortal Kombat'
     },
-    {question:
-		{text: 'Who says "@!#?@!" every time he is hit by something?',
+}, {
+    question: {
+        text: 'Who says "@!#?@!" every time he is hit by something?',
         gif: "https://api.giphy.com/v1/gifs/search?q=qbert&api_key=dc6zaTOxFJmzC",
         answers: ['Donkey Kong', 'Wimpy', 'Bagman', 'Q*bert'],
-		correctAnswer: 'Q*bert'
-		},
-    }
-]
+        correctAnswer: 'Q*bert'
+    },
+}]
 
 new Quiz(questions);
-
-// Switch to next available question --------------------------------
-function nextQuestion() {
-    if (answeredQuestions === 6) {
-        displayResults();
-    } else if (answeredQuestions === 5) {
-        $('#question-text').html(availableQuestions[5].question.text);
-        answer = availableQuestions[5].question.correctAnswer
-        for (let i = 0; i < availableQuestions[5].question.answers.length; i++) {
-            j = $('<button>');
-            j.addClass('btn btn-md btn-default btn-block answer');
-            j.text(availableQuestions[5].question.answers[i]);
-            $('#answers').append(j);
-        }
-    } else if (answeredQuestions === 4) {
-        $('#question-text').html(availableQuestions[4].question.text);
-        answer = availableQuestions[4].question.correctAnswer
-        for (let i = 0; i < availableQuestions[4].question.answers.length; i++) {
-            j = $('<button>');
-            j.addClass('btn btn-md btn-default btn-block answer');
-            j.text(availableQuestions[4].question.answers[i]);
-            $('#answers').append(j);
-        }
-    } else if (answeredQuestions === 3) {
-        $('#question-text').html(availableQuestions[3].question.text);
-        answer = availableQuestions[3].question.correctAnswer
-        for (let i = 0; i < availableQuestions[3].question.answers.length; i++) {
-            j = $('<button>');
-            j.addClass('btn btn-md btn-default btn-block answer');
-            j.text(availableQuestions[3].question.answers[i]);
-            $('#answers').append(j);
-        }
-    } else if (answeredQuestions === 2) {
-        $('#question-text').html(availableQuestions[2].question.text);
-        answer = availableQuestions[2].question.correctAnswer
-        for (let i = 0; i < availableQuestions[2].question.answers.length; i++) {
-            j = $('<button>');
-            j.addClass('btn btn-md btn-default btn-block answer');
-            j.text(availableQuestions[2].question.answers[i]);
-            $('#answers').append(j);
-        }
-    } else if (answeredQuestions === 1) {
-        $('#question-text').html(availableQuestions[1].question.text);
-        answer = availableQuestions[1].question.correctAnswer
-        for (let i = 0; i < availableQuestions[1].question.answers.length; i++) {
-            j = $('<button>');
-            j.addClass('btn btn-md btn-default btn-block answer');
-            j.text(availableQuestions[1].question.answers[i]);
-            $('#answers').append(j);
-        }
-    } else {
-        $('#question-text').html(availableQuestions[0].question.text);
-        answer = availableQuestions[0].question.correctAnswer;
-        for (let i = 0; i < availableQuestions[0].question.answers.length; i++) {
-            j = $('<button>');
-            j.addClass('btn btn-md btn-default btn-block answer');
-            j.text(availableQuestions[0].question.answers[i]);
-            $('#answers').append(j);
-        }
-    }
-}
--- js/script.js
+++ js/script.js
@@ -2,18 +2,27 @@ $(document).ready(function() {
     // FUNCTIONS
     // -----------------------------------------------------------------------
 
-    // Countdown timer for questions ------------------------------------
+    // Countdown timers for questions and asnwer ------------------------
     function questionTimer() {
         counter = setInterval(decrement, 1000);
     }
 
+    function answerTimer() {
+        counter = setInterval(decrement, 1000);
+    }
+
     function decrement() {
-        questionTime--;
-        $('#countdown').html(`Time Remaining: ${questionTime} seconds`);
+        if (onQuestion === true) {
+            questionTime--;
+            $('#countdown').html(`Time Remaining: ${questionTime} seconds`);
+            console.log(questionTime);
+        } else {
+            answerTime--;
+            console.log(`Answer Time: ${answerTime}`);
+        }
 
         if (questionTime === 0) {
             numUnanswered++;
-
             stopTimer();
             $('#game-display').hide();
             $('#choice').html("You took to long to answer. :/");
@@ -22,11 +31,19 @@ $(document).ready(function() {
 
             // Testing console -------------------------------
             console.log('Unanswered: ' + numUnanswered);
+        } else if (answerTime === 0) {
+            console.log('Time Up');
+            stopTimer();
+            nextQuestion();
         }
     }
 
-    function resetTimer() {
+    function resetQuestionTimer() {
         questionTime = 30;
+    }
+
+    function resetAnswerTimer() {
+        answerTime = 10;
     }
 
     function stopTimer() {
@@ -49,19 +66,130 @@ $(document).ready(function() {
             array[m] = array[i];
             array[i] = t;
         }
-
         return array;
+    }
+
+    // Switch to next available question --------------------------------
+    function nextQuestion() {
+        onQuestion = true;
+        questionTimer();
+        resetAnswerTimer();
+        $('#countdown').html('Time Remaining: 30 seconds')
+        $('#answer').hide();
+        $('#game-display').show();
+        if (answeredQuestions === 6) {
+            displayResults();
+        } else if (answeredQuestions === 5) {
+            $('#question-text').html(availableQuestions[5].question.text);
+            answer = availableQuestions[5].question.correctAnswer
+            for (let i = 0; i < availableQuestions[5].question.answers.length; i++) {
+                j = $('<button>');
+                j.addClass('btn btn-md btn-default btn-block answer');
+                j.text(availableQuestions[5].question.answers[i]);
+                $('#answers').append(j);
+            }
+        } else if (answeredQuestions === 4) {
+            $('#question-text').html(availableQuestions[4].question.text);
+            answer = availableQuestions[4].question.correctAnswer
+            for (let i = 0; i < availableQuestions[4].question.answers.length; i++) {
+                j = $('<button>');
+                j.addClass('btn btn-md btn-default btn-block answer');
+                j.text(availableQuestions[4].question.answers[i]);
+                $('#answers').append(j);
+            }
+        } else if (answeredQuestions === 3) {
+            $('#question-text').html(availableQuestions[3].question.text);
+            answer = availableQuestions[3].question.correctAnswer
+            for (let i = 0; i < availableQuestions[3].question.answers.length; i++) {
+                j = $('<button>');
+                j.addClass('btn btn-md btn-default btn-block answer');
+                j.text(availableQuestions[3].question.answers[i]);
+                $('#answers').append(j);
+            }
+        } else if (answeredQuestions === 2) {
+            $('#question-text').html(availableQuestions[2].question.text);
+            answer = availableQuestions[2].question.correctAnswer
+            for (let i = 0; i < availableQuestions[2].question.answers.length; i++) {
+                j = $('<button>');
+                j.addClass('btn btn-md btn-default btn-block answer');
+                j.text(availableQuestions[2].question.answers[i]);
+                $('#answers').append(j);
+            }
+        } else if (answeredQuestions === 1) {
+            $('#question-text').html(availableQuestions[1].question.text);
+            answer = availableQuestions[1].question.correctAnswer
+            for (let i = 0; i < availableQuestions[1].question.answers.length; i++) {
+                j = $('<button>');
+                j.addClass('btn btn-md btn-default btn-block answer');
+                j.text(availableQuestions[1].question.answers[i]);
+                $('#answers').append(j);
+            }
+        } else {
+            $('#question-text').html(availableQuestions[0].question.text);
+            answer = availableQuestions[0].question.correctAnswer;
+            for (let i = 0; i < availableQuestions[0].question.answers.length; i++) {
+                j = $('<button>');
+                j.addClass('btn btn-md btn-default btn-block answer');
+                j.text(availableQuestions[0].question.answers[i]);
+                $('#answers').append(j);
+            }
+        }
     }
 
     // Display correct answer -------------------------------------------
     function displayAnswer() {
-        $('#correct-answer').html(`The correct answer is: ${questions[0].question.correctAnswer}`);
-        $.ajax({
-            url: questions[0].question.gif,
-            method: 'GET'
-        }).done(function(response) {
-            $('#gif').attr('src', response.data[0].images.fixed_height.url);
-        });
+        if (answeredQuestions === 6) {
+            $('#correct-answer').html(`The correct answer is: ${availableQuestions[5].question.correctAnswer}`);
+            $.ajax({
+                url: availableQuestions[5].question.gif,
+                method: 'GET'
+            }).done(function(response) {
+                $('#gif').attr('src', response.data[0].images.fixed_height.url);
+            });
+        } else if (answeredQuestions === 5) {
+            $('#correct-answer').html(`The correct answer is: ${availableQuestions[4].question.correctAnswer}`);
+            $.ajax({
+                url: availableQuestions[4].question.gif,
+                method: 'GET'
+            }).done(function(response) {
+                $('#gif').attr('src', response.data[0].images.fixed_height.url);
+            });
+        } else if (answeredQuestions === 4) {
+            $('#correct-answer').html(`The correct answer is: ${availableQuestions[3].question.correctAnswer}`);
+            $.ajax({
+                url: availableQuestions[3].question.gif,
+                method: 'GET'
+            }).done(function(response) {
+                $('#gif').attr('src', response.data[0].images.fixed_height.url);
+            });
+        } else if (answeredQuestions === 3) {
+            $('#correct-answer').html(`The correct answer is: ${availableQuestions[2].question.correctAnswer}`);
+            $.ajax({
+                url: availableQuestions[2].question.gif,
+                method: 'GET'
+            }).done(function(response) {
+                $('#gif').attr('src', response.data[0].images.fixed_height.url);
+            });
+        } else if (answeredQuestions === 2) {
+            $('#correct-answer').html(`The correct answer is: ${availableQuestions[1].question.correctAnswer}`);
+            $.ajax({
+                url: availableQuestions[1].question.gif,
+                method: 'GET'
+            }).done(function(response) {
+                $('#gif').attr('src', response.data[0].images.fixed_height.url);
+            });
+        } else if (answeredQuestions === 1) {
+            $('#correct-answer').html(`The correct answer is: ${availableQuestions[0].question.correctAnswer}`);
+            $.ajax({
+                url: availableQuestions[0].question.gif,
+                method: 'GET'
+            }).done(function(response) {
+                $('#gif').attr('src', response.data[0].images.fixed_height.url);
+            });
+        }
+        answerTimer();
+        console.log(availableQuestions[0].question.gif);
+        console.log(availableQuestions[0].question.correctAnswer);
     }
 
     // Initialize the game with a start page ----------------------------
@@ -79,6 +207,7 @@ $(document).ready(function() {
         questionTime = 30;
         answerTime = 10;
         counter = '';
+        onQuestion = false;
         numRight = 0;
         numWrong = 0;
         numUnanswered = 0;
@@ -92,38 +221,35 @@ $(document).ready(function() {
             shuffle(availableQuestions[i].question.answers);
         }
 
-        $('#countdown').html('Time Remaining: 30 seconds')
-        $('#game-display').show();
         $('#start').hide();
-        $('#results').hide();
-        questionTimer();
         nextQuestion();
     });
 
     // Check if selected answer is wrong/right --------------------------
     $(document).on('click', '.answer', function() {
+        onQuestion = false;
         $('.answer').remove();
         answeredQuestions++;
         if (this.innerHTML === answer) {
             numRight++;
-            // $('#game-display').hide();
-            // $('#choice').html("That's right!");
-            // $('#answer').show();
+            $('#game-display').hide();
+            $('#choice').html("That's right!");
+            $('#answer').show();
             // Testing console -------------------------------------
             console.log('Right: ' + numRight);
         } else {
             numWrong++;
-            // $('#game-display').hide();
-            // $('#choice').html("Sorry, that's incorrect. :(");
-            // $('#answer').show();
+            $('#game-display').hide();
+            $('#choice').html("Sorry, that's incorrect. :(");
+            $('#answer').show();
             // Testing console -------------------------------------
             console.log('Wrong: ' + numWrong);
         }
-        // stopTimer();
-
+        stopTimer();
+        resetQuestionTimer();
         console.log(`Answered Questions: ${answeredQuestions}`);
-        // displayAnswer();
-        nextQuestion();
+        console.log(onQuestion);
+        displayAnswer();
     });
 
     // Display results --------------------------------------------------
